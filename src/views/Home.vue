1c3f0550c5cec18f1c520c5f148f453e71c97d0e
Fuyutsuki <glacierluna@gmail.com>
2020-04-24 16:39:18 +0800

    feat: 实现分页功能

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,19 @@
 <template>
   <div class="home" v-if="posts">
+    <!--文章预览组件-->
     <PostPreview
-      v-for="(post,index) in posts"
+      v-for="(post,index) in currentPosts"
       :key="post._id"
       :post="post"
       :index="index"
-      @delete-post="deletePost"
+      @delete-post="handleDelete"
     />
+    <!--element 分页组件-->
+    <el-pagination
+      layout="prev, pager, next"
+      :total="posts.length"
+      @current-change="handleCurrentPageChange"
+    ></el-pagination>
   </div>
 </template>
 
@@ -18,21 +25,37 @@ export default {
   components: { PostPreview },
   data() {
     return {
-      posts: null
+      posts: null, // 所有文章的数据
+      currentPage: 1 // 当前页数
     };
   },
+  computed: {
+    currentPosts() {
+      let firstIndex = (this.currentPage - 1) * 10;
+      let lastIndex = firstIndex + 10;
+      return this.posts.slice(firstIndex, lastIndex);// slice方法切片数组
+    } // 当前页数对应的文章数据，每页10条数据
+  },
   methods: {
-    deletePost(index) {
+    handleDelete(index) {
       deletePost(this.posts[index]._id);
       this.posts.splice(index, 1);
-    }
+    }, 
+    handleCurrentPageChange(currentPage) {
+      this.currentPage = currentPage;
+    } // 跳转页数的事件处理，把跳转到的页数设置为当前页数
   },
   mounted() {
     getPosts()
       .then(response => (this.posts = response.data.reverse()))
-      .catch(err => console.log(err));
+      .catch(err => console.log(err)); // 初始化所有文章的数据
   }
 };
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.el-pagination {
+  text-align: center;
+  margin-top: 10px;
+}
+</style>
