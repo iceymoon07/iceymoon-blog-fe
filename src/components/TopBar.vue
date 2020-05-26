61f523d8376856ef8e36b5dc51d7359a9e51af1d
Fuyutsuki <glacierluna@gmail.com>
2020-05-26 15:28:51 +0800

    feat: 顶栏样式更新

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="top-bar">
-    <span @click="loginDialogVisible=true" v-if="!isLogin">登录</span>
-    <span v-if="isLogin">已登录</span>
-    <span @click="$router.push('/newpost')" v-if="isLogin">写文章</span>
-    <span v-if="isLogin" @click="handleLogout">退出登录</span>
+    <span class="top-bar-label--login" @click="loginDialogVisible=true" v-if="!isLogin">登录</span>
+    <span class="top-bar-label--logged" style="color:red" v-if="isLogin">已登录</span>
+    <span class="top-bar-label--newpost" @click="$router.push('/newpost')" v-if="isLogin">写文章</span>
+    <span class="top-bar-label--logout" v-if="isLogin" @click="handleLogout">退出登录</span>
     <el-dialog title="登录" :visible.sync="loginDialogVisible">
       <el-form :model="loginForm">
         <el-form-item label="用户名" label-width="120px">
@@ -28,9 +28,6 @@ import { mapMutations, mapState } from "vuex";
 export default {
   data() {
     return {
-      search: "",
-      fontPreviewText: "</code>",
-      value: "",
       loginDialogVisible: false,
       loginForm: {
         name: "",
@@ -75,7 +72,17 @@ export default {
   align-items: center;
 
   > span {
-    margin-right: 5px;
+    font-weight: bold;
+    margin-right: 10px;
+  }
+
+  .top-bar-label--login,
+  .top-bar-label--newpost,
+  .top-bar-label--logout {
+    &:hover {
+      text-decoration: underline;
+      cursor: pointer;
+    }
   }
 
   .el-input {
